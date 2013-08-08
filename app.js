73493416ed817b15895650a8491108aea29eb71f
web@ns300141.ovh.net <ns300141.ovh.net>
2013-08-08 19:02:50 +0200

    commit php

---
+++ app.js
@@ -0,0 +1,57 @@
+﻿
+//$(document).ready(function () {
+//  document.addEventListener("deviceready", onDeviceReady, false);
+//});
+
+var application;
+
+function initializeViewModel() {
+  // create the view model
+  application = new ApplicationViewModel();
+
+  application.currentViewModel.subscribe(function (viewModel) {
+    if (viewModel !== undefined) {
+      $("#app").empty();
+      $("#" + viewModel.template).tmpl("").appendTo("#app");
+      ko.applyBindings(viewModel);
+    }
+  });
+
+  application.backButtonRequired.subscribe(function (backButtonRequired) {
+    if (backButtonRequired) {
+      document.addEventListener("backbutton", onBackButton, false);
+    } else {
+      document.removeEventListener("backbutton", onBackButton, false);
+    }
+  });
+
+  application.navigateTo(new TwitterSearchViewModel());
+
+  window.external.Notify("getState");
+
+
+//  $(document).scroll(function () {
+//    if (!vm.selectedTweet()) {
+//      vm.scrollPosition = $(document).scrollTop();
+//    }
+//  });
+}
+
+//function onDeviceReady() {
+//  initializeViewModel();
+//}
+
+function onBackButton() {
+  application.back();
+}
+
+// returns the view model state
+function getState() {
+  return application.getState();
+}
+
+// sets the view model state
+function setState(state) {
+  application.setState(state);
+}
+
---
+++ applicationViewModel.js
@@ -0,0 +1,50 @@
+﻿
+function ApplicationViewModel() {
+
+  var that = this;
+
+  this.viewModelBackStack = ko.observableArray();
+
+  this.backButtonRequired = ko.dependentObservable(function () {    
+    return this.viewModelBackStack().length > 1;
+  }, this);
+
+  this.currentViewModel = ko.dependentObservable(function () {
+    return this.viewModelBackStack()[this.viewModelBackStack().length-1];
+  }, this);
+
+  this.navigateTo = function (viewModel) {
+    this.viewModelBackStack.push(viewModel);
+  }
+
+  this.back = function () {
+    this.viewModelBackStack.pop();
+  }
+
+  // gets the view model state as a JSON string
+  this.getState = function () {
+    var state = ko.observableArray();
+    for (var i = 0; i < that.viewModelBackStack().length; i++) {
+      var viewModel = that.viewModelBackStack()[i];
+      state.push({
+        "factoryName" : viewModel.factoryName,
+        "viewModel" : viewModel
+      })
+    }
+    return ko.toJSON(state);
+  }
+
+  // sets the view model state based on the given JSON string.
+  this.setState = function (stateString) {
+    var state = $.parseJSON(stateString);
+
+    that.viewModelBackStack.removeAll();
+    for (var i = 0; i < state.length; i++) {
+      var viewModelState = state[i];
+      var factoryName = viewModelState.factoryName;
+      var viewModel = window[factoryName](viewModelState.viewModel);
+      that.viewModelBackStack.push(viewModel);
+    };
+  }
+
+}
